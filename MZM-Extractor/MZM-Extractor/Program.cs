8e7d83b48d96db44a87cfb5fbf1fa5112445f8e5
YohannDR <yohann.demaison-regne@orange.fr>
2022-03-03 21:59:07 +0100

    Initial commit

---
+++ MZM-Extractor/MZM-Extractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MZM_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
